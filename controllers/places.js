9ba71f94f2c9bed5fdc514bdc5ac0ab89e6167e9
okayzach <znortyy@gmail.com>
2022-05-11 15:48:33 -0400

    New places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -1,24 +1,27 @@
 const router = require('express').Router()
+const places = require('../models/places.js')
 
 router.get('/', (req, res) => {
-    let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: '/images/thai-food.jpg'
-      }, {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: '/images/cat-cafe.jpg'
-      }]
     res.render('./places/index', { places })
 })
 
 router.get('/new', (req, res) => {
   res.render('places/new')
+})
+
+router.post('/', (req, res) => {
+  if (!req.body.pic) {
+    // Default image if one is not provided
+    req.body.pic = 'http://placekitten.com/400/400'
+  }
+  if (!req.body.city) {
+    req.body.city = 'Anytown'
+  }
+  if (!req.body.state) {
+    req.body.state = 'USA'
+  }
+  places.push(req.body)
+  res.redirect('/places')
 })
 
 // router.get('/:id', (req, res) => {
--- index.js
+++ index.js
@@ -5,8 +5,9 @@ const app = express()
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
-
 app.use('/places', require('./controllers/places'))
+app.use(express.urlencoded({ extended: true }))
+
 
 app.get('/', (req, res) => {
     res.render('home')
