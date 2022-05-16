5ca86915f63eee51697777fe4c16e8cb1d99dda6
okayzach <znortyy@gmail.com>
2022-05-16 14:02:44 -0400

    Added edit functionality but redirect odes not work

--- controllers/places.js
+++ controllers/places.js
@@ -9,6 +9,50 @@ router.get('/new', (req, res) => {
   res.render('places/new')
 })
 
+router.get('/:id', (req, res) => {
+  res.render('places/show', { place: places[`${req.params.id}`] })
+})
+
+router.get('/:id/edit', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+      res.render('error404')
+  }
+  else if (!places[id]) {
+      res.render('error404')
+  }
+  else {
+    res.render('places/edit', { place: places[id] })
+  }
+})
+
+router.put('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+      res.render('error404')
+  }
+  else if (!places[id]) {
+      res.render('error404')
+  }
+  else {
+      // Dig into req.body and make sure data is valid
+      if (!req.body.pic) {
+          // Default image if one is not provided
+          req.body.pic = 'http://placekitten.com/400/400'
+      }
+      if (!req.body.city) {
+          req.body.city = 'Anytown'
+      }
+      if (!req.body.state) {
+          req.body.state = 'USA'
+      }
+
+      // Save the new data into places[id]
+      places[id] = req.body
+      res.redirect(`/places/${id}`)
+  }
+})
+
 router.delete('/places/:id', (req, res) => {
   let id = Number(req.params.id)
   if (isNaN(id)) {
@@ -38,17 +82,6 @@ router.post('/', (req, res) => {
   res.redirect('/places')
 })
 
-router.get('/:id', (req, res) => {
-  let id = Number(req.params.id)
-  if (isNaN(id)) {
-    res.render('error404')
-  }
-  else if (!places[id]) {
-    res.render('error404')
-  }
-  else {
-    res.render('places/show', { place: places[id], id })
-  }
-})
+
 
 module.exports = router
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -6,6 +6,16 @@ function show (data) {
         <Def>
           <main>
             <h1>{ data.place.name }</h1>
+            <img src={data.place.pic} alt={`${data.place.name}'s Lobby`} width="75%"/>
+            <div id="rating">
+                <h2>Rating</h2>
+                <p>Not Rated</p>
+            </div>
+            <div>
+                <h2>Description</h2>
+                <p>Located in {data.place.city}, {data.place.state}</p>
+                <p>Serving {data.place.cuisines}</p>
+            </div>
             <a href={`/places/${data.id}/edit`} className="btn btn-warning">Edit</a>
                 <form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
                     <button type="submit" className="btn btn-danger">
